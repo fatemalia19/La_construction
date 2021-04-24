46f8cad1481547ebddc16f610299ba63593a0a33
fatemalia19 <83101302+fatemalia19@users.noreply.github.com>
2021-04-24 07:21:58 +0600

    Add files via upload

---
+++ about.php
@@ -0,0 +1,350 @@
+<!doctype html>
+<html class="no-js" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>La Construcción</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- All css files are included here. -->
+    <!-- Bootstrap fremwork main css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <!-- Owl Carousel  main css -->
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <!-- This core.css file contents all plugings css file. -->
+    <link rel="stylesheet" href="css/core.css">
+    <!-- Theme shortcodes/elements style -->
+    <link rel="stylesheet" href="css/shortcode/shortcodes.css">
+    <!-- Theme main style -->
+    <link rel="stylesheet" href="style.css">
+    <!-- Responsive css -->
+    <link rel="stylesheet" href="css/responsive.css">
+    <!-- User style -->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Modernizr JS -->
+    <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+</head>
+<body>
+    <!-- Body main wrapper start -->
+    <div class="wrapper">
+        <!-- Start Header Style -->
+        <div id="header" class="htc-header">
+            <!-- Start Header Top -->
+            <div class="htc__header__top bg__cat--1">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6 col-lg-6 col-sm-6 col-xs-12">
+                            <div class="header__top__right">
+                                <ul class="login-register">
+                                    <li><a href="login-register.html">LOGIN</a></li>
+                                    <li class="separator">/</li>
+                                    <li><a href="login-register.html">REGISTER</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- End Header Top -->
+            <!-- Start Mainmenu Area -->
+            <div id="sticky-header-with-topbar" class="mainmenu__wrap sticky__header">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-2 col-sm-6 col-xs-7">
+                            <div class="logo">
+                                    <img src="images/favicon.ico" alt="logo image">La Construcción
+                            </div>
+                        </div>
+                        <div class="col-md-8 col-sm-6 col-xs-5">
+                            <nav class="main__menu__nav  hidden-xs hidden-sm">
+                                <ul class="main__menu">
+                                    <li class="drop"><a href="index.html">HOME</a>
+                                    </li>
+                                    <li><a href="about.html">ABOUT</a></li>
+                                    <li><a href="service.html">SERVICES</a></li>
+                                    <li class="drop"><a href="#">Media Center</a>
+                                        <ul class="dropdown">
+                                            <li><a href="photos.html">Photos</a></li>
+                                            <li><a href="#">Videos</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="drop"><a href="#">PROJECTS</a>
+                                        <ul class="dropdown">
+                                            <li><a href="projects-one.html">Completed projects</a></li>
+                                            <li><a href="projects-two.html">On going projects</a></li>
+                                        </ul>
+                                    </li>
+                                    <li class="drop"><a href="research&investor.html">Research & Investor</a>
+                                    </li>
+                                    <li><a href="contact.html">CONTACT</a></li>
+                                </ul>
+                            </nav>
+                            <div class="mobile-menu clearfix visible-xs visible-sm">
+                                <nav id="mobile_dropdown">
+                                    <ul>
+                                        <li><a href="index.html">Home</a>
+                                        </li>
+                                        <li><a href="about.html">ABOUT</a></li>
+                                        <li><a href="#">PROJECTS</a>
+                                            <ul>
+                                                <li><a href="projects-one.html">Completed projects</a></li>
+                                                <li><a href="projects-two.html">On going projects</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">Media Center</a>
+                                            <ul>
+                                                <li><a href="photos.html">Photos</a></li>
+                                                <li><a href="#">Videos</a></li>
+                                            </ul>
+                                        </li> 
+                                        <li><a href="research&investor.html">Research & Investor</a>
+                                        </li>
+                                        <li><a href="contact.html">contact</a></li>
+                                    </ul>
+                                </nav>
+                            </div> 
+                        </div>
+                        <div class="col-md-2 col-sm-6 hidden-xs">
+                            <div class="htc__header__search">
+                                <input type="text" placeholder="SEARCH">
+                                <a href="#"><i class="fa fa-search"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mobile-menu-area"></div>
+                </div>
+            </div>
+            <!-- End Mainmenu Area -->
+        </div>
+        <!-- End Header Style -->
+        <!-- Start Bradcaump area -->
+        <div class="ht__bradcaump__area" data--black__overlay="6" style="background: rgba(0, 0, 0, 0) url(images/bg/5.jpg) no-repeat scroll center center / cover ;">
+            <div class="ht__bradcaump__wrap">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="bradcaump__inner text-center">
+                                <h2 class="bradcaump-title">About Us</h2>
+                                <nav class="bradcaump-inner">
+                                  <a class="breadcrumb-item" href="index.html">Home</a>
+                                  <span class="brd-separetor">-</span>
+                                  <span class="breadcrumb-item active">About Us</span>
+                                </nav>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <!-- End Bradcaump area -->
+        <!-- Start Offer Area -->
+        <section class="htc__mission__area bg__gray">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-9 col-lg-5 col-sm-7 col-xs-12">
+                        <div class="htc__mission__container">
+                            <div class="single__mission">
+                                <h2>OUR MISSION</h2>
+                                <p>To perform for our customers the highest level of quality construction services at fair and market competitive prices. 
+                                    To maintain the highest levels of professionalism, integrity, honesty and fairness in our relationships with our suppliers, subcontractors, professional associates and customers. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="htc__offer__thumb">
+                <div class="htc__offer__thumb__inner">
+                    <img src="images/about/1.jpg" alt="offer img">
+                </div>
+            </div>
+        </section>
+        <br><br><br><br><br><br><br><br><br>
+        <!-- End Offer Area -->
+        <!-- Start Team Area -->
+        <section class="htc__team__area pb--120 bg__white">
+            <div class="htc__team__container">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="section__title text-center">
+                                <h2 class="title__line">MD YEAQUB ALI <br> CEO of the Company </h2>
+                                <img src="" alt="">
+                                <p> He Say: <br> Beginning our journey in 2000, we set up a course to establish a name that will eventually become one of the most trusted business houses in the country.
+                                    Our elemental strength is our ability to adopt the present day challenges while we also attend to our individual responsibilities to the society and our nation at large.
+                                    <br> We have a duty towards our country, the Government being one of our key stakeholders.All relevant taxes and duties are a paid in time and all our businesses are conducted 
+                                    in adherence to the rules and regulations applicable for the businesses. <br> MD YEAQUB ALI <br> CEO, La Construcción </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Team Area -->
+        <!-- Start Testimonial Area -->
+        <section class="htc__testimonial__area ptb--130" data--gray__overlay="9" style="background: rgba(0, 0, 0, 0) url(images/bg/1.jpg) no-repeat fixed center center / cover ;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-lg-6  col-lg-offset-1 col-sm-12 col-xs-12 smt-40">
+                        <div class="reguest__quote">
+                            <div class="section__title text-left">
+                                <h2 class="title__line">REQUEST A <span class="text--theme">QUOTE</span></h2>
+                                <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasur</p>
+                            </div>
+                            <form id="contact-form" action="https://d29u17ylf1ylz9.cloudfront.net/simply-construction/mail-faq.php" method="post">
+                                <div class="clint__comment__form">
+                                    <div class="single__cl__form">
+                                        <input name="name" type="text" placeholder="Name" id="nm">
+                                        <input name="email" type="email" placeholder="Email" id="email">
+                                    </div>
+                                    <div class="single__cl__form">
+                                        <input name="telephone" type="tel" placeholder="Phone" id="tel">
+                                        <input name="subject" type="text" placeholder="Subject" id="sub">
+                                    </div>
+                                    <div class="single__cl__message">
+                                        <textarea name="message" placeholder="Massage" id="mess"></textarea>
+                                    </div>
+                                    <div class="clint__submit__btn">
+                                        <button class="submit htc__btn" type="submit">
+                                            SEND MESSAGE 
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                            <p class="form-messege"></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Testimonial Area -->
+        <!-- Start Contact Address -->
+        <div class="htc__contact__address add-res bg__cat--1">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="htc__contact__wrap clearfix">
+                            <!-- Start Single Address -->
+                            <div class="ht__address__inner">
+                                <div class="ht__address__icon map-pin">
+                                    <i class="zmdi zmdi-pin"></i>
+                                </div>
+                                <div class="ht__address__details">
+                                    <p>House No-69/D/1 <br>Dhanmondi, Dhaka</p>
+                                </div>
+                            </div>
+                            <!-- End Single Address -->
+                            <!-- Start Single Address -->
+                            <div class="ht__address__inner">
+                                <div class="ht__address__icon">
+                                    <i class="fa fa-phone" aria-hidden="true"></i>
+                                </div>
+                                <div class="ht__address__details">
+                                    <p><a href="#">01712190270</a></p>
+                                    <p><a href="#">01822109054</a></p>
+                                </div>
+                            </div>
+                            <!-- End Single Address -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Contact Address -->
+        <!-- Start Footer Area -->
+        <footer class="htc__footer__area">
+            <div class="footer__top ptb--130" data--1f2d30__overlay="9.5" style="background: rgba(0, 0, 0, 0) url(images/bg/2.jpg) no-repeat fixed center center / cover ;">
+                <div class="container">
+                    <div class="row">
+                        <div class="htc__footer__wrap clearfix">
+                            <!-- Start Single Footer -->
+                            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12">
+                                <div class="footer">
+                                    <div class="footer__widget">
+                                        <h2 class="ft__title">ABOUT</h2>
+                                    </div>
+                                    <div class="ft__details">
+                                        <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled demoralized On the other hand, we denounce </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Footer -->
+                            <!-- Start Single Footer -->
+                            <div class="col-md-3 col-lg-3 col-sm-6 col-xs-12 xmt-40">
+                                <div class="footer quick__link">
+                                    <div class="footer__widget">
+                                        <h2 class="ft__title">QUICK LINKS</h2>
+                                    </div>
+                                    <div class="footer__link">
+                                        <ul class="ft__menu">
+                                            <li><a href="service.html">Services</a></li>
+                                            <li><a href="projects-one.html">Projects</a></li>
+                                            <li><a href="research&investor.html">Research & Investor</a></li>
+                                        </ul>
+                                        <ul class="ft__menu">
+                                            <li><a href="photos.html">Media Center</a></li>
+                                            <li><a href="projects-one.html">Projects</a></li>
+                                            <li><a href="contact.html">Contact</a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Footer -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright bg__theme">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xs-12">
+                            <div class="copyright__inner">
+                                <p>Copyright <a href="#" target="_blank">Devitems</a>
+                                All Rights Reserved 2017</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+        <!-- End Footer Area -->
+    </div>
+    <!-- jquery latest version -->
+    <script src="js/vendor/jquery-1.12.0.min.js"></script>
+    <!-- All js plugins included in this file. -->
+    <script src="js/plugins.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <!-- Main js file that contents all jQuery plugins activation. -->
+    <script src="js/main.js"></script>
+    <?php
+    $name="";
+    $email="";
+    $telephone="";
+    $subject="";
+    $message="";
+
+    $db = mysqli_connect ('localhost','root','','la_construction');
+
+    if(isset($_POST['contact-form'])){
+        echo "hello after";
+        $name =mysqli_real_escape_string($db, $_POST['nm']);
+        $email =mysqli_real_escape_string($db, $_POST['email']);
+        $telephone =mysqli_real_escape_string($db,$_POST['tel']);
+        $subject =mysqli_real_escape_string($db, $_POST['sub']);
+        $message =mysqli_real_escape_string($db, $_POST['mess']);
+        
+        $q= "INSERT INTO message (nm ,email , tel, sub, mess) VALUES ('$name','$email','$telephone','$subject','$message' )";
+        mysqli_query($db,$q);
+        header('location: about.php');
+}     
+    
+    
+    ?>
+</body>
+</html>
